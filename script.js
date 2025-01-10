febd4a1836f35a8bfb8ea415b20c8cc503f996d3
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-10 10:08:22 +0100

    changing the joke api

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 async function getJoke() {
   try {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const response = await fetch("https://official-joke-api.appspot.com/random_joke");
     const data = await response.json();
     jokeElement.innerHTML = data.value;
+    console.log(data)
+    // document.getElementById('joke').innerHTML = `${data.setup}- ${data.punchline}`
   } catch (error) {
     jokeElement.innerHTML = "Oops! Couldn't fetch a joke. Try again.";
     console.log(error);
