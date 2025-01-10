00821d605989af289b26f1d2696346ebb6682f0a
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-10 09:41:33 +0100

    done

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// script.js
-
 const jokeElement = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
 
-async function getJoke(){
-    try{
-        const response = await fetch('https://api.chucknorris.io/jokes/random')
-        const data = await response.json ()
-        jokeElement.innerHTML = data.value
-    }catch(e)
+async function getJoke() {
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const data = await response.json();
+    jokeElement.innerHTML = data.value;
+  } catch (error) {
+    jokeElement.innerHTML = "Oops! Couldn't fetch a joke. Try again.";
+    console.log(error);
+  }
 }
+jokeBtn.addEventListener("click", getJoke);
+
+getJoke();
